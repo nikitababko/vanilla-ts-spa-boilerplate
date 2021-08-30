b92e8cb108a2d65338a068c6908943e15e19f3c9
Nikita Babko <niki.babko@gmail.com>
2021-08-30 19:53:34 +0700

    Added the ability to change the page when changing the address

--- src/pages/app/index.ts
+++ src/pages/app/index.ts
@@ -1,19 +1,50 @@
+import Page from '../../core/templates/page';
 import MainPage from '../main';
 import SettingsPage from '../settings';
 import StatisticsPage from '../statistics';
 
+export const enum PageIds {
+  MainPage = 'main-page',
+  SettingsPage = 'settings-page',
+  StatisticsPage = 'statistics-page',
+}
+
 class App {
-  private container: HTMLElement;
+  private static container: HTMLElement = document.body;
   private initialPage: MainPage;
 
+  static renderNewPage(idPage: string) {
+    App.container.innerHTML = '';
+    let page: Page | null = null;
+
+    if (idPage === PageIds.MainPage) {
+      page = new MainPage(idPage);
+    } else if (idPage === PageIds.SettingsPage) {
+      page = new SettingsPage(idPage);
+    } else if (idPage === PageIds.StatisticsPage) {
+      page = new StatisticsPage(idPage);
+    }
+
+    if (page) {
+      const pageHTML = page.render();
+      App.container.append(pageHTML);
+    }
+  }
+
+  private enableRouteChange() {
+    window.addEventListener('hashchange', () => {
+      const hash = window.location.hash.slice(1);
+      App.renderNewPage(hash);
+    });
+  }
+
   constructor() {
-    this.container = document.body;
     this.initialPage = new MainPage('main-page');
   }
 
   run() {
-    const mainPageHTML = this.initialPage.render();
-    this.container.append(mainPageHTML);
+    App.renderNewPage('main-page');
+    this.enableRouteChange();
   }
 }
 
